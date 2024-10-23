acd99a2b6770f71a870fbfe0fc53c86aa056bd45
Phr_Noon <noon009mot@gmail.com>
2024-10-23 17:59:54 +0700

    update on get wishlist and pargination

--- src/controllers/users/wishlist-controller.js
+++ src/controllers/users/wishlist-controller.js
@@ -1,4 +1,15 @@
-const {Wishlist, User} = require('../../models');
+const {
+  Product,
+  Wishlist,
+  User,
+  ProductSubCategory,
+  BrandCategorySub,
+  BrandCategory,
+  MainCategory,
+  SubCategory,
+  ProductDiscount,
+  Discount,
+} = require('../../models');
 const resMsg = require('../../config/messages');
 
 exports.createWishlist = async (req, res, next) => {
@@ -45,19 +56,81 @@ exports.deleteWishlist = async (req, res, next) => {
   }
 };
 exports.getMyWishlist = async (req, res, next) => {
+  const page = parseInt(req.query.page) || 1;
+  const pageSize = parseInt(req.query.pageSize) || 4;
+  const order = req.query.order || 'ASC';
   try {
-    const result = await Wishlist.findAll({
+    const {count, rows} = await Wishlist.findAndCountAll({
+      where: {userId: req.user.id},
       include: [
         {
-          model: User,
-          where: {
-            id: req.user.id,
-          },
+          model: Product,
+          attributes: [
+            'id',
+            'title',
+            'productImage',
+            'price',
+            'qtyInStock',
+            'description',
+            'slug',
+          ],
+          include: [
+            {
+              model: ProductSubCategory,
+              attributes: ['id'],
+              required: true,
+              include: [
+                {
+                  model: BrandCategorySub,
+                  attributes: ['id'],
+                  required: true,
+                  include: [
+                    {
+                      model: SubCategory,
+                      attributes: ['slug'],
+                      required: true,
+                    },
+                    {
+                      model: BrandCategory,
+                      attributes: ['id'],
+                      required: true,
+                      include: [
+                        {
+                          model: MainCategory,
+                          required: true,
+                          attributes: ['slug'],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              model: ProductDiscount,
+              include: [
+                {
+                  model: Discount,
+                },
+              ],
+            },
+          ],
         },
       ],
+      order: [['createdAt', order]],
+      limit: pageSize,
+      offset: (page - 1) * pageSize,
     });
-    res.status(200).json({...resMsg.getMsg(200), result});
+    res.status(200).json({
+      ...resMsg.getMsg(200),
+      count: count,
+      totalPages: Math.ceil(count / pageSize),
+      currentPage: page,
+      result: rows,
+    });
   } catch (err) {
+    console.log(err);
+
     res.status(500).json(resMsg.getMsg(500));
   }
 };
